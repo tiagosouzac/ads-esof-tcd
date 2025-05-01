0e4b2786df42a1a389074f334f59fefd0e1d7829
Tiago Castro <tiagoscastro602@gmail.com>
2025-05-01 00:38:46 +0000

    configure `prisma orm`

---
+++ backend/config/database.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "../infra/database/generated/prisma";
+
+class Database {
+  private static instance: PrismaClient;
+
+  private constructor() {}
+
+  static getInstance(): PrismaClient {
+    if (!Database.instance) {
+      Database.instance = new PrismaClient();
+    }
+
+    return Database.instance;
+  }
+
+  static async connect() {
+    const client = Database.getInstance();
+
+    try {
+      await client.$connect();
+      console.log("Database connected");
+    } catch (error) {
+      console.error("Error connecting to the database", error);
+    }
+  }
+
+  static async disconnect() {
+    if (Database.instance) {
+      try {
+        await Database.instance.$disconnect();
+        console.log("Database disconnected");
+      } catch (error) {
+        console.error("Error disconnecting from the database", error);
+      }
+    }
+  }
+}
+
+export { Database };
