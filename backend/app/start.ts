b700b3132604ed484ef08c62033cee567303e86b
Tiago Castro <tiagoscastro602@gmail.com>
2025-05-01 12:19:59 +0000

    configure environment variables validator

--- backend/app/start.ts
+++ backend/app/start.ts
@@ -1,9 +1,11 @@
+import { Env } from "../config/env";
 import { App } from "./app";
 
 const main = async () => {
+  await Env.load();
   const app = new App();
   await app.init();
-  app.start(3000);
+  app.start(Env.get("PORT"));
 };
 
 main();
---
+++ backend/config/env.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+class Env {
+  private static schema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]),
+    PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.string(),
+    JWT_SECRET: z.string(),
+  });
+
+  private static env: z.infer<typeof this.schema>;
+
+  static load() {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        process.loadEnvFile();
+        this.validate();
+        console.log("🌍 Environment variables loaded successfully!");
+        resolve();
+      } catch (error) {
+        console.error("❌ Failed to load environment variables:", error);
+        reject(error);
+      }
+    });
+  }
+
+  private static validate() {
+    const result = this.schema.safeParse(process.env);
+
+    if (!result.success) {
+      console.error(
+        "❌ Environment variables validation failed:",
+        result.error.flatten().fieldErrors
+      );
+
+      throw new Error("Invalid environment variables");
+    }
+
+    this.env = result.data;
+  }
+
+  static get<Key extends keyof typeof this.env>(key: Key) {
+    return this.env[key] satisfies (typeof this.env)[Key];
+  }
+}
+
+export { Env };
